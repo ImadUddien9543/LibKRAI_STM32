df7eed573a173576f74a613273760885dbb3638f
ImadUddien9543 <113409331+ImadUddien9543@users.noreply.github.com>
2024-10-20 14:07:58 +0700

    Update Generate_LUT.c

--- misc/Generate_LUT.c
+++ misc/Generate_LUT.c
@@ -6,25 +6,24 @@
   This is a file to generate LUT points for Linear Interpolation using LUT method
 */
 
-#define LUT_POINTS_12BIT 24
-#define LUT_POINTS_16BIT 64
+#define LUT_POINTS 2048 //values of LUT points
 
 const uint16_t MAX_12BIT_VAL = 4095;
 const uint16_t MAX_16BIT_VAL = 65535;
 
-uint16_t LUT_12BIT[LUT_POINTS_12BIT];
-uint16_t LUT_16BIT[LUT_POINTS_16BIT];
+uint16_t LUT_12BIT[LUT_POINTS];
+uint16_t LUT_16BIT[LUT_POINTS];
 
 int main(int argc, char const *argv[])
 {
-	for(int i = 0; i < LUT_POINTS_12BIT; i++){
-		LUT_12BIT[i] = (uint16_t)((i * MAX_12BIT_VAL) / (LUT_POINTS_12BIT - 1));
+	for(int i = 0; i < LUT_POINTS; i++){
+		LUT_12BIT[i] = (uint16_t)((i * MAX_12BIT_VAL) / (LUT_POINTS - 1));
 		// printf("LUT_12BIT[i] = %d\n", LUT_12BIT[i]);
     // printf("%d, ", LUT_12BIT[i]);
 	}
 
-	for(int j = 0; j < LUT_POINTS_16BIT; j++){
-		LUT_16BIT[j] = (uint16_t)((j * MAX_16BIT_VAL) / (LUT_POINTS_16BIT - 1));
+	for(int j = 0; j < LUT_POINTS; j++){
+		LUT_16BIT[j] = (uint16_t)((j * MAX_16BIT_VAL) / (LUT_POINTS - 1));
 		// printf("LUT_16BIT[%d] = %d\n",j, LUT_16BIT[j]);
     // printf("%d, ", LUT_16BIT[i]);
 	}
