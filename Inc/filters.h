f46b1eb605c9d4719301b26b5c951da4809c0dfc
ImadUddien9543 <113409331+ImadUddien9543@users.noreply.github.com>
2024-10-21 15:11:03 +0700

    Create filters.h

---
+++ Inc/filters.h
@@ -0,0 +1,34 @@
+/*
+ * filters.h
+ *
+ *  Created on: Oct 21, 2024
+ *      Author: Imaduddien
+ */
+
+#ifndef INC_FILTERS_H_
+#define INC_FILTERS_H_
+
+#include "stm32f4xx_hal.h"
+#include "stdint.h"
+#include "stdlib.h"
+#include "stdbool.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+typedef struct {
+	float alpha, beta;
+	float now_output, prev_ouput;
+	uint32_t start_time, last_time, delta_time, sample_time;
+} Filter_t;
+
+void init_LowPass(filter *f, float alpha, float beta, float sample_time);
+float get_LowPass(filter *f, float input);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* INC_FILTERS_H_ */
